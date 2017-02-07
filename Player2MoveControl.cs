d68f0f12776ce695f3d006579e41f676361a6e1a
Sam0nline <Sam0nline@users.noreply.github.com>
2017-02-07 14:20:16 +0000

    Player2MoveControl.cs

---
+++ Player2MoveControl.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+public class Player2MoveControl : MonoBehaviour {
+//Generic class name for secondary player movement
+
+    public float speed = 5f;
+    public bool IsGrounded = false;
+    public Rigidbody2D rb;
+	
+    // Update is called once per frame
+    void Update()
+    {
+        var movementx = new Vector3 (Input.GetAxis ("Vertical"), 0, 0);
+        transform.position += movementx * speed * Time.deltaTime;
+
+        if (Input.GetKey (KeyCode.Return) && IsGrounded==true)
+        {
+            rb.velocity = new Vector3(0, 5, 0);
+            IsGrounded = false;
+        }
+    }   
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Platform")
+        {
+            IsGrounded = true;
+        }
+    } 
+}
