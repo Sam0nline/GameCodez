647f55254aa556298cee7570a51591a5e2391192
Sam0nline <Sam0nline@users.noreply.github.com>
2017-02-24 12:54:26 +0000

    Invoking and repeating spawns of objects
    The spawner can have several objects spawn from it at once.

---
+++ SpawnBlockz.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBlockz : MonoBehaviour {
+
+    public GameObject BlockSpawner;
+    public float spawnTime = 25;
+    int time = 50;
+    public Transform[] spawnPoints;
+
+	void Start ()
+    {
+        
+    }
+
+	void Update ()
+    {
+        spawnTime--;
+        if (spawnTime < 0) {
+            
+            int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+            Instantiate(BlockSpawner, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+            spawnTime = time;
+        }
+    }
+}
