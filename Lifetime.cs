e5a01325c4fe422d3eb3a6f76e66e900c97e5e64
Sam0nline <Sam0nline@users.noreply.github.com>
2017-02-24 12:50:14 +0000

    Simple game object destruction after time elapse

---
+++ Lifetime.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lifetime : MonoBehaviour
+{
+    public float lifeTime = 50;
+
+    void Update()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+}
